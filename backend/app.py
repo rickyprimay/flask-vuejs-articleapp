b76caab62c0fff399cdcbaa72d13744eccbd6c55
Ricky Primayuda Putra <113336156+rickyprimay@users.noreply.github.com>
2024-01-14 20:54:07 +0700

    completed endpoint to serve to frontend

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,15 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, abort
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 import pytz
+from flask_marshmallow import Marshmallow
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:root@localhost:5432/flask_article"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+ma = Marshmallow(app)
 
 class Article(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -18,14 +20,76 @@ class Article(db.Model):
     def __init__(self, title, body):
         self.title = title
         self.body = body
+
+class ArticleSchema(ma.Schema):
+    class Meta:
+        fields  = ('id', 'title', 'body', 'date')
+
+article_schema = ArticleSchema()
+articles_schema = ArticleSchema(many=True)
+
 with app.app_context():
     db.create_all()
 
-@app.route('/', methods=["GET"])
-def get_articles():
-    return jsonify({
-        "msg":"starter"
-    })
+@app.route('/get', methods=["GET"])
+def get_article():
+    all_articles = Article.query.all()
+    if all_articles:
+        results = articles_schema.dump(all_articles)
+        return jsonify(results)
+    else:
+        return jsonify({"message": "No articles found"})
+
+@app.route('/get/<int:id>/', methods=["GET"])
+def post_details(id):
+    article = Article.query.get(id)
+    
+    if article:
+        return article_schema.jsonify(article)
+    else:
+        return abort(404, description="Article not found")
+
+@app.route('/add', methods=["POST"])
+def add_article():
+    title = request.json.get('title')
+    body = request.json.get('body')
+
+    if title and body:
+        articles = Article(title, body)
+        db.session.add(articles)
+        db.session.commit()
+        return article_schema.jsonify(articles)
+    else:
+        return jsonify({"message": "Title and body are required"}), 400
+
+@app.route('/update/<int:id>/', methods=["PUT"])
+def update_article(id):
+    article = Article.query.get(id)
+
+    if article:
+        title = request.json.get('title')
+        body = request.json.get('body')
+
+        if title and body:
+            article.title = title
+            article.body = body
+            db.session.commit()
+            return article_schema.jsonify(article)
+        else:
+            return jsonify({"message": "Title and body are required"}), 400
+    else:
+        return abort(404, description="Article not found")
+
+@app.route('/delete/<int:id>/', methods=["DELETE"])
+def delete_article(id):
+    article = Article.query.get(id)
+
+    if article:
+        db.session.delete(article)
+        db.session.commit()
+        return article_schema.jsonify(article)
+    else:
+        return abort(404, description="Article not found")
 
 if __name__ == '__main__':
     app.run(debug=True, port=5001)
